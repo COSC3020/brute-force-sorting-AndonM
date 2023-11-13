1179dc6deec6715c555ed8462663b5649077fc72
Andon Mefford <andonmefford@outlook.com>
2023-11-12 20:35:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,36 @@
+// A COUNTEREXAMPLE OF [1, 0] FAILS WITH THIS EVERY TIME
+
 function permutationSort(a) {
-    return 0;
+    function isSorted(arr) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function swap(arr, i, j) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    function permute(arr, start, count) {
+        if (start === arr.length) {
+            if (isSorted(arr)) {
+                return count;
+            }
+            return count + 1;
+        }
+
+        let totalTries = count;
+        for (let i = start; i < arr.length; i++) {
+            swap(arr, start, i);
+            totalTries = permute([...arr], start + 1, totalTries);
+            swap(arr, start, i);
+        }
+        return totalTries;
+    }
+    return permute(a.slice(), 0, 0);
 }
