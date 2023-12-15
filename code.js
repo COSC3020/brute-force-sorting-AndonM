6f4f478b173bff610a5db64ae3949774e6221030
Andon Mefford <andonmefford@outlook.com>
2023-12-14 20:26:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,44 @@
 function permutationSort(arr) {
-    function isSorted(arr) {
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                return false;
+    let originalArray = [...arr]; 
+    let permutations = []; 
+    let perms = getPermutations(arr, 0, permutations);
+    let sortedFound = false;
+    for (let permutationsAt = 0; permutationsAt < perms.length; permutationsAt++) {
+        if (isSorted(perms[permutationsAt])) {
+            sortedFound = true;
+            for (let j = 0; j < perms[permutationsAt].length; j++) {
+                arr[j] = perms[permutationsAt][j];
             }
         }
-        return true;
     }
+    return perms.length;
+}
 
-    function swap(arr, i, j) {
-        const temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
-    function permute(arr, start, count) {
-        if (start === arr.length) {
-            if (isSorted(arr)) {
-                return count;
-            }
-            return count + 1;
+function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
         }
-        let totalTries = count;
+    }
+    return true;
+}
+
+function getPermutations(arr, start, count) {
+    if (start === arr.length) {
+        count.push([...arr]);
+    } 
+    else {
         for (let i = start; i < arr.length; i++) {
             swap(arr, start, i);
-            totalTries = permute([...arr], start + 1, totalTries);
+            getPermutations(arr, start + 1, count);
             swap(arr, start, i);
         }
-        return totalTries;
     }
-    return permute(arr.slice(), 0, 0);
+    return count; 
 }
